e9d66f71b9bc0d35f72ad763950d300ac9455790
Alfanisa <fannysafvira@gmail.com>
2023-03-22 14:29:56 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    // eslint-disable-next-line no-undef
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    // eslint-disable-next-line no-undef
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+// eslint-disable-next-line eol-last
+module.exports = routes;
